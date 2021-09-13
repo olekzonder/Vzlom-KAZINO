083a25f45e00568fc3f9a43fbc227673c8263f66
Alexander Smirnov <olekzonder@outlook.com>
2021-09-13 10:52:22 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Window1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(218, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Да";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(49, 269);
+            this.progressBar1.Maximum = 125;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(688, 43);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(1, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(796, 65);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Выкачанно 0 руб. из 70050505 руб.";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(1, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(796, 70);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Казино взломано на 100%";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(445, 343);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 56);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Нет";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(1, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(796, 65);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Продолжить?";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Window1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Window1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Взлом КАЗИНО";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Window1 : Form
+    {
+        int FinalNumber, Counter, Part;
+        double Percent, CurrentPercent;
+        public Window1()
+        {
+            InitializeComponent();
+            Random random = new Random();
+            FinalNumber = random.Next(5000000, 7500000);
+            FinalNumber = FinalNumber - FinalNumber % 125;
+            Counter = 0;
+            Part = FinalNumber / 125;
+            progressBar1.Step = Part;
+            Percent = (double)Part / FinalNumber;
+            CurrentPercent = 0;
+            label1.Text = "Казино взломанно на " + CurrentPercent.ToString("0%") + ".";
+            label2.Text = "Выкачанно " + Counter.ToString() + " руб. из " + FinalNumber.ToString() + " руб.";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (progressBar1.Value < 125)
+            {
+                progressBar1.Value++;
+                CurrentPercent += Percent;
+                Counter += Part;
+                label1.Text = "Казино взломанно на " + CurrentPercent.ToString("0%") + ".";
+                label2.Text = "Выкачанно " + Counter.ToString() + " руб. из " + FinalNumber.ToString() + " руб.";
+                            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+       "всм",
+       "чел ты ошибся кнопкой",
+       MessageBoxButtons.OK,
+       MessageBoxIcon.Error,
+       MessageBoxDefaultButton.Button1,
+       MessageBoxOptions.DefaultDesktopOnly);
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
